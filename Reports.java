f25c870b4cae3d10d4d07bca14f312313061b616
kgoshman1 <75673437+kgoshman1@users.noreply.github.com>
2021-03-10 16:15:50 -0800

    Add files via upload

--- Reports.java
+++ Reports.java
@@ -31,6 +31,8 @@ public class Reports implements Initializable {
     Button home;
     @FXML
     TextArea textArea1;
+    @FXML
+    TextArea textArea2;
 
     public void mainMenu(javafx.event.ActionEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("Main_Menu.fxml"));
@@ -76,6 +78,50 @@ public class Reports implements Initializable {
         textArea1.setText(String.valueOf(stringBuilder));
     }
 
+    public void scheduleReport(ActionEvent event) throws SQLException {
+        StringBuilder stringBuilder = new StringBuilder();
+        String query = "SELECT Contact_Name as 'Contact_Name', Appointment_ID as 'Appointment_ID', Title as 'Title', Type as 'Type', Description as 'Description', Start as 'Start', " +
+                "End as 'End', Customer_ID as 'Customer_ID' FROM contacts INNER JOIN appointments GROUP BY Start, Appointment_ID, Contact_Name, Title, Type, Description, Start, End " +
+        " ORDER BY Contact_Name, Start;";
+        ResultSet rs = dbConnection.conn.createStatement().executeQuery(query);
+        while (rs.next()) {
+            stringBuilder.append(rs.getString("Contact_Name"))
+                    .append("                ")
+                    .append(rs.getString("Appointment_ID"))
+                    .append("                ")
+                    .append(rs.getString("Title"))
+                    .append("                ")
+                    .append(rs.getString("Type"))
+                    .append("                           ")
+                    .append(rs.getString("Description"))
+                    .append("                           ")
+                    .append(rs.getString("Start"))
+                    .append("                           ")
+                    .append(rs.getString("End"))
+                    .append("                           ")
+                    .append(rs.getString("Customer_ID"))
+                    .append("           \n");
+
+        }
+        textArea2.setText(String.valueOf(stringBuilder));
+    }
+
+    public void otherReport(ActionEvent event) throws SQLException {
+        StringBuilder stringBuilder = new StringBuilder();
+        String query = "SELECT Type as 'Type', count(*) as 'Count' \n" +
+                "FROM appointments\n" +
+                "GROUP BY Type \n" +
+                "ORDER BY count(*) DESC;";
+        ResultSet rs = dbConnection.conn.createStatement().executeQuery(query);
+        while (rs.next()) {
+            stringBuilder.append(rs.getString("Type"))
+                    .append("           ")
+                    .append(rs.getString("Count"))
+                    .append("           \n");
+
+        }
+        textArea1.setText(String.valueOf(stringBuilder));
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
